b547913cdbaeb872ad5105a6d425881dbe2a1f35
Tom Parfitt <tparfitt7@gmail.com>
2025-12-01 21:03:18 +0000

    day 01 cleanup

--- aoc2025/day01/day01.py
+++ aoc2025/day01/day01.py
@@ -34,7 +34,7 @@ def question2():
             if remainder != 0:
                 new_position = prev_position - remainder if direction == 'L' else prev_position + remainder
                 partial_turns = 0 if prev_position == 0 or 0 < new_position < 100 else 1
-                prev_position = abs(100 + new_position) % 100
+                prev_position = new_position % 100
 
             password += full_turns + partial_turns
 
